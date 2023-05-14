643f74c5947f66e7c7140f46afca5b355082d6bf
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-05-14 18:37:44 -0300

    Feat: Profile route controller and adding jwt auth

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,15 @@
 import { FastifyInstance } from 'fastify';
+
 import { register } from './controllers/register';
 import { authenticate } from './controllers/authenticate';
+import { profile } from './controllers/profile';
+import { verifyJWT } from './middlewares/verify-jwt';
 
 export async function appRouter(app: FastifyInstance) {
   app.post('/users', register);
+
   app.post('/sessions', authenticate);
+
+  /**Authenticated */
+  app.get('/me', { onRequest: [verifyJWT] } ,profile);
 }
