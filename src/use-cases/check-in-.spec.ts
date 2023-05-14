7b62e58a1c9a582a56859388bdedea0d03eb1664
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-05-14 12:31:35 -0300

    Feat: Finishing checkin and gym useCases and tests

--- src/use-cases/check-in-.spec.ts
+++ src/use-cases/check-in-.spec.ts
@@ -1,24 +1,105 @@
-import { beforeEach, describe, expect, it } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { Decimal } from '@prisma/client/runtime/library';
 
 import { CheckInUseCase } from './check-in';
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository';
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository';
+import { MaxNumberOfCheckInsError } from './errors/max-number-of-check-ins-error';
+import { MaxDistanceError } from './errors/max-distance-error';
 
 let checkInRepository: InMemoryCheckInsRepository;
+let gymsRepository: InMemoryGymsRepository;
 let sut: CheckInUseCase;
 
 describe('Check in Profile Use Case', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     checkInRepository = new InMemoryCheckInsRepository();
-    sut = new CheckInUseCase(checkInRepository);
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new CheckInUseCase(checkInRepository, gymsRepository);
+
+    await gymsRepository.create({
+      id: 'gym-1',
+      title: 'Gym test',
+      description: 'Description',
+      phone: '111',
+      latitude: 0,
+      longitude:  0,
+    });
+
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
   });
 
   it('Should be able to check in', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+
     const { checkIn } = await sut.execute({
       userId: 'user-1',
       gymId: 'gym-1',
+      userLatitude: 0,
+      userLongitude: 0
     });
 
     expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it('Should not be able to check in twice in the same day', async () => {
+    await sut.execute({
+      userId: 'user-1',
+      gymId: 'gym-1',
+      userLatitude: 0,
+      userLongitude: 0
+    });
+
+    await expect(() => sut.execute({
+      userId: 'user-1',
+      gymId: 'gym-1',
+      userLatitude: 0,
+      userLongitude: 0
+    })).rejects.toBeInstanceOf(MaxNumberOfCheckInsError);
+  });
+
+  it('Should be able to check in twice in the different days', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+
+    await sut.execute({
+      userId: 'user-1',
+      gymId: 'gym-1',
+      userLatitude: 0,
+      userLongitude: 0
+    });
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0));
+
+    const { checkIn } = await sut.execute({
+      userId: 'user-1',
+      gymId: 'gym-1',
+      userLatitude: 0,
+      userLongitude: 0
+    });
+
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it('Should not be able to check in', async () => {
+    gymsRepository.items.push({
+      id: 'gym-2',
+      title: 'Gym test',
+      description: 'Description',
+      phone: '111',
+      latitude: new Decimal(-23.5211345),
+      longitude:  new Decimal(-46.5814662),
+    });
+
+    await expect(() => sut.execute({
+      userId: 'user-1',
+      gymId: 'gym-2',
+      userLatitude: -23.5067319,
+      userLongitude: -46.6714345,
+    })).rejects.toBeInstanceOf(MaxDistanceError);
   });
 });
 
