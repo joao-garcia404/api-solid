1b254e3e6696470c6c7bc18917a46f792be32f9c
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-05-14 14:37:16 -0300

    Feat: Check-in prisma repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -6,7 +6,13 @@ import { UsersRepository } from '../users-repository';
 
 export class PrismaUsersRepository implements UsersRepository {
   async findById(id: string): Promise<User | null> {
-    throw new Error('Method not implemented.');
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      }
+    });
+
+    return user;
   }
 
   async findUserByEmail(email: string) {
