bf51de04d550a195232d79994d90e8cf8d32a615
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-05-20 13:39:57 -0300

    Feat: Authenticate e2e test

---
+++ src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,36 @@
+import request from 'supertest';
+import { app } from '@/app';
+
+import { describe, it, expect, beforeAll, afterAll } from 'vitest';
+
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('Should be able to authenticate', async () => {
+    await request(app.server)
+      .post('/users')
+      .send({
+        name: 'Test name',
+        email: 'e2e-authenticate@email.com',
+        password: '123456'
+      });
+
+    const response = await request(app.server)
+      .post('/sessions')
+      .send({
+        email: 'e2e-authenticate@email.com',
+        password: '123456'
+      });
+
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toEqual({
+      token: expect.any(String)
+    });
+  });
+});
